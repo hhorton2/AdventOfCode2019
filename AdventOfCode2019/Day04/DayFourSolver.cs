e65ba7edb7128a033ed01c48b235922035000dfd
Hunter Horton <hunter.horton@hexagonsi.com>
2019-12-04 09:15:56 -0600

    Day 4

---
+++ AdventOfCode2019/Day04/DayFourSolver.cs
@@ -0,0 +1,92 @@
+﻿namespace AdventOfCode2019.Day04
+{
+    public class DayFourSolver : ISolver
+    {
+        public string PartOneSolve(string input)
+        {
+            var rangeNums = input.Split("-");
+            var min = int.Parse(rangeNums[0]);
+            var max = int.Parse(rangeNums[1]);
+            var passwordMatchCount = 0;
+            for (var i = min; i <= max; i++)
+            {
+                var currentNum = i.ToString().ToCharArray();
+                var hasMatch = false;
+                var allNumbersGreater = true;
+                for (var j = 1; j < currentNum.Length; j++)
+                {
+                    if (int.Parse(currentNum[j].ToString()) < int.Parse(currentNum[j - 1].ToString()))
+                    {
+                        allNumbersGreater = false;
+                        break;
+                    }
+
+                    if (currentNum[j] == currentNum[j - 1])
+                    {
+                        hasMatch = true;
+                    }
+                }
+
+                if (hasMatch && allNumbersGreater)
+                {
+                    passwordMatchCount++;
+                }
+            }
+
+            return $"{passwordMatchCount}";
+        }
+
+        public string PartTwoSolve(string input)
+        {
+            var rangeNums = input.Split("-");
+            var min = int.Parse(rangeNums[0]);
+            var max = int.Parse(rangeNums[1]);
+            var passwordMatchCount = 0;
+            for (var i = min; i <= max; i++)
+            {
+                var currentNum = i.ToString().ToCharArray();
+                var hasMatch = false;
+                var allNumbersGreater = true;
+                var matchCount = 0;
+                for (var j = 1; j < currentNum.Length; j++)
+                {
+                    if (int.Parse(currentNum[j].ToString()) < int.Parse(currentNum[j - 1].ToString()))
+                    {
+                        allNumbersGreater = false;
+                        break;
+                    }
+
+                    if (currentNum[j] == currentNum[j - 1])
+                    {
+                        matchCount++;
+                        if (j == currentNum.Length - 1)
+                        {
+                            matchCount = CheckMatchCountAndReset(matchCount, ref hasMatch);
+                        }
+                    }
+                    else
+                    {
+                        matchCount = CheckMatchCountAndReset(matchCount, ref hasMatch);
+                    }
+                }
+
+                if (hasMatch && allNumbersGreater)
+                {
+                    passwordMatchCount++;
+                }
+            }
+
+            return $"{passwordMatchCount}";
+        }
+
+        private static int CheckMatchCountAndReset(int matchCount, ref bool hasMatch)
+        {
+            if (matchCount == 1)
+            {
+                hasMatch = true;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2019Test/Day04/DayFourSolver_should_.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2019.Day04;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2019Test.Day04
+{
+    public class DayFourSolver_should_
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public DayFourSolver_should_(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+        [Fact]
+        public void solve_for_unknown_input_part_one()
+        {
+            var solver = new DayFourSolver();
+            
+            _outputHelper.WriteLine(solver.PartOneSolve("271973-785961"));
+        }
+        [Fact]
+        public void solve_for_unknown_input_part_two()
+        {
+            var solver = new DayFourSolver();
+            
+            _outputHelper.WriteLine(solver.PartTwoSolve("271973-785961"));
+        }
+    }
+}
