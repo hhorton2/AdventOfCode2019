5df2ccb86e01800e6a74c78466d07f4361afdb9a
Hunter Horton <hhorton@atlasrfid.com>
2019-12-08 11:32:26 -0600

    Day 7 Part 1

--- AdventOfCode2019/Day07/DaySevenSolver.cs
+++ AdventOfCode2019/Day07/DaySevenSolver.cs
@@ -1,15 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using AdventOfCode2019.Intcode;
+
 namespace AdventOfCode2019.Day07
 {
-    public class DaySevenSolver: ISolver
+    public class DaySevenSolver : ISolver
     {
         public string PartOneSolve(string input)
         {
-            throw new System.NotImplementedException();
+            var rom = new ReadOnlyCollection<int>(input.Split(",").Select(int.Parse).ToArray());
+            var computer = new IntcodeComputer();
+            var inputs = new Queue<int>();
+            var maxThrust = 0;
+            for (var a = 0; a < 5; a++)
+            {
+                for (var b = 0; b < 5; b++)
+                {
+                    for (var c = 0; c < 5; c++)
+                    {
+                        for (var d = 0; d < 5; d++)
+                        {
+                            for (var e = 0; e < 5; e++)
+                            {
+                                var nums = new[]
+                                {
+                                    a, b, c, d, e
+                                };
+                                var numSet = new HashSet<int>
+                                {
+                                    a, b, c, d, e
+                                };
+                                if (nums.Length > numSet.Count)
+                                {
+                                    continue;
+                                }
+
+                                inputs.Enqueue(a);
+                                inputs.Enqueue(0);
+                                var aOutput = computer.Compute(rom.ToArray(), inputs);
+                                inputs.Enqueue(b);
+                                inputs.Enqueue(int.Parse(aOutput.First()));
+                                var bOutput = computer.Compute(rom.ToArray(), inputs);
+                                inputs.Enqueue(c);
+                                inputs.Enqueue(int.Parse(bOutput.First()));
+                                var cOutput = computer.Compute(rom.ToArray(), inputs);
+                                inputs.Enqueue(d);
+                                inputs.Enqueue(int.Parse(cOutput.First()));
+                                var dOutput = computer.Compute(rom.ToArray(), inputs);
+                                inputs.Enqueue(e);
+                                inputs.Enqueue(int.Parse(dOutput.First()));
+                                var eOutput = computer.Compute(rom.ToArray(), inputs);
+                                var currentThrust = int.Parse(eOutput.First());
+                                if (currentThrust > maxThrust)
+                                {
+                                    maxThrust = currentThrust;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return maxThrust.ToString();
         }
 
         public string PartTwoSolve(string input)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
--- AdventOfCode2019Test/Day07/DaySevenSolver_should_.cs
+++ AdventOfCode2019Test/Day07/DaySevenSolver_should_.cs
@@ -1,7 +1,43 @@
+using System.IO;
+using AdventOfCode2019.Day07;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
 namespace AdventOfCode2019Test.Day07
 {
     public class DaySevenSolver_should_
     {
-        
+        private readonly ITestOutputHelper _outputHelper;
+
+        public DaySevenSolver_should_(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+        [Theory]
+        [InlineData("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", "43210")]
+        [InlineData("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0", "54321")]
+        [InlineData(
+            "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+            "65210")]
+        public void solve_part_one_for_known_values(string program, string expected)
+        {
+            var solver = new DaySevenSolver();
+
+            var thrust = solver.PartOneSolve(program);
+
+            thrust.Should().Be(expected);
+        }
+
+        [Fact]
+        public void solve_for_unknown_value()
+        {
+            var inputs = File.ReadAllLines("./Day07/input_part_one.txt");
+            var solver = new DaySevenSolver();
+
+            var actual = solver.PartOneSolve(inputs[0]);
+
+            _outputHelper.WriteLine($"{actual}");
+        }
     }
 }
