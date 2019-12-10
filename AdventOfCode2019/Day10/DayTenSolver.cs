fef5c3766154de89dcadc53639c074480585e488
Hunter Horton <hunter@keysys.io>
2019-12-10 12:47:20 -0600

    Preparing for day 10

---
+++ AdventOfCode2019/Day10/DayTenSolver.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2019.Day10
+{
+    public class DayTenSolver : ISolver
+    {
+        public string PartOneSolve(string input)
+        {
+            /* TIPS FOR FUTURE ME
+             * m = y2-y1 / x2-x1
+             * y = mx + b
+             * b = y - mx
+             *
+             * Vertical line has no slope - EDGE CASE
+             */
+            throw new System.NotImplementedException();
+        }
+
+        public string PartTwoSolve(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2019Test/Day10/DayTenSolver_should_.cs
@@ -0,0 +1,7 @@
+namespace AdventOfCode2019Test.Day10
+{
+    public class DayTenSolver_should_
+    {
+        
+    }
+}
